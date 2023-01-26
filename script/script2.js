cf6a882fef862bab40b81af0fde677e4055472e2
Cleiver <creibis10@gmail.com>
2023-01-26 11:56:16 -0300

    apenas exibicao de quizz implementado

--- script/script2.js
+++ script/script2.js
@@ -3,14 +3,72 @@ let IdQuizzSelecionado = window.location.hash.substring(1);
 const url = "https://mock-api.driven.com.br/api/v4/buzzquizz";
 let quizzSelecionado;
 
-function adicionaNoHtml(){
-    const perguntas = document.querySelector(".perguntas");
+let tituloPergunta;
+let corPergunta;
 
-    let templatePergunta;
+const backgroundImage = "linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%),";
+
+function mudaFundoQuizz (urlImg){
+    const imgDom = document.querySelector(".fundo-quizz");
+    imgDom.style.backgroundImage = `${backgroundImage} url('${urlImg}')`;
+    // console.log(imgDom);
+
+    const h2Dom = document.querySelector(".fundo-quizz h2");
+    h2Dom.innerHTML = quizzSelecionado.title;
 }
 
-function renderizarQuizzSelecionado(){
+function montarRespostas(resposta){
+
+    let templateResposta = `<div class="imagens-respostas">
+                                <div>
+                                    <img src="${resposta.image}" alt="imagem-resposta">
+                                    <p>${resposta.text}</p>
+                                </div>
+                            </div>`;
+
+
+    // document.querySelector(".mensagens mensagem:last-child");
+    const perguntaNoDom = document.querySelector(".perguntas-container .pergunta:last-child");
+    const imagemRespostas = perguntaNoDom.lastElementChild;
+    imagemRespostas.innerHTML += templateResposta;
+
+    // var ultimoFilho = pai.lastElementChild;
+
+    const corDeFundoTitulo = perguntaNoDom.firstElementChild;
+
+    // console.log(corDeFundoTitulo);
+    corDeFundoTitulo.style.backgroundColor = corPergunta;
+}
+
+function adicionaNoHtml(){
     
+    const perguntas = document.querySelector(".perguntas");
+    console.log(quizzSelecionado);
+
+
+    mudaFundoQuizz(quizzSelecionado.image);
+    console.log(quizzSelecionado.questions);
+    quizzSelecionado.questions.forEach((pergunta) => {
+
+        corPergunta = pergunta.color;
+        tituloPergunta = pergunta.title;
+        console.log("COR DA PERGUNTA BACKGROUND COLOR: " + corPergunta);
+    
+
+        const respostas = pergunta.answers;
+        respostas.sort( () => Math.random() - 0.5)
+        const templatePergunta = `<div class="pergunta">
+                                    <div class="texto-pergunta"">
+                                        <p>${tituloPergunta}</p>
+                                    </div>
+                                    <div class="imagens-respostas">
+                                    </div>
+                                </div>`;
+
+        const perguntaNoDom = document.querySelector(".perguntas-container");
+        perguntaNoDom.innerHTML += templatePergunta;
+        respostas.forEach((resposta) => montarRespostas(resposta));
+    });
 }
 
 function buscarQuizzSelecionado(quizz){
@@ -24,7 +82,7 @@ function sucessoObterQuizzes(callback){
 
     quizzes.forEach(buscarQuizzSelecionado);
 
-    renderizarQuizzSelecionado();
+    adicionaNoHtml();
 }
 
 function erroObterQuizzes(){
@@ -39,4 +97,3 @@ function obterQuizzes(){
 }
 
 obterQuizzes();
-// console.log(IdQuizz);
