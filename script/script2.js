de0f38259b3d6c2699555fa2f72796602046604e
Cleiver <creibis10@gmail.com>
2023-01-26 14:11:36 -0300

    adicionado funcionalidade de responder - retorno é certo falta implementar fundo escuro

--- script/script2.js
+++ script/script2.js
@@ -3,15 +3,72 @@ let IdQuizzSelecionado = window.location.hash.substring(1);
 const url = "https://mock-api.driven.com.br/api/v4/buzzquizz";
 let quizzSelecionado;
 
+let IdPergunta = 1;
+
 let tituloPergunta;
 let corPergunta;
 
+let desempenhoRespostas = [];
+
 const backgroundImage = "linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%),";
+
+
+function verificarResposta(resposta){
+    // se a resposta foi dada não há como mudar
+    if(resposta.parentNode.classList.contains("respondido")){
+        return null;
+    }
+
+    // adiciona uma forma de saber se deu a resposta anteriormente
+    resposta.parentNode.classList.add("respondido");
+    desempenhoRespostas.push(resposta.classList[0]);
+
+
+    let perguntaSelecionada;
+
+    // adiciona opacidade para a carta selecionada
+    resposta.classList.add("selecionada");
+
+    const perguntasDom = document.querySelectorAll(".perguntas-container .pergunta");
+    // procurar no Dom qual foi a pergunta selecionada para modificar suas propriedades
+    for(let i = 0; i < perguntasDom.length; i++){
+        if(perguntasDom[i] == resposta.parentNode.parentNode){
+            perguntaSelecionada = perguntasDom[i];
+        }
+    }
+
+    // procura a resposta certa e deixa seu texto verde
+    const listaRespostas = perguntaSelecionada.lastElementChild.children;
+    for(let j = 0; j < listaRespostas.length; j++){
+        // console.log(listaRespostas[0].classList[0]);
+        if(listaRespostas[j].classList[0] == "true"){
+            // console.log("ACHEI A RESPOSTA CERTA!");
+            // console.log(listaRespostas[j].classList[0]);
+            listaRespostas[j].classList.add("certa");
+        }else{
+            listaRespostas[j].classList.add("errada");
+        }
+    }
+    for(let j = 0; j < listaRespostas.length; j++){
+        // se não contem a classe selecionada adiciona filtro
+        console.log(listaRespostas[j].classList);
+        if(!(listaRespostas[j].classList.contains("selecionada"))){
+            listaRespostas[j].classList.add("resposta-nao-selecionada");
+        }
+    }
+    console.log(listaRespostas);
+
+    // com essa lista consigo descrever o desemppenho que o usuario teve no quizz
+    console.log(desempenhoRespostas);
+
+
+
+   
+}
 
 function mudaFundoQuizz (urlImg){
     const imgDom = document.querySelector(".fundo-quizz");
     imgDom.style.backgroundImage = `${backgroundImage} url('${urlImg}')`;
-    // console.log(imgDom);
 
     const h2Dom = document.querySelector(".fundo-quizz h2");
     h2Dom.innerHTML = quizzSelecionado.title;
@@ -19,13 +76,12 @@ function mudaFundoQuizz (urlImg){
 
 function montarRespostas(resposta){
 
-    let templateResposta = `<div class="imagens-respostas">
-                                <div>
-                                    <img src="${resposta.image}" alt="imagem-resposta">
-                                    <p>${resposta.text}</p>
-                                </div>
-                            </div>`;
+    const boolResposta = resposta.isCorrectAnswer; 
 
+    let templateResposta = `<div onclick="verificarResposta(this)" class="${boolResposta} " >
+                                <img src="${resposta.image}" alt="imagem-resposta">
+                                <p>${resposta.text}</p>
+                            </div>`;
 
     // document.querySelector(".mensagens mensagem:last-child");
     const perguntaNoDom = document.querySelector(".perguntas-container .pergunta:last-child");
@@ -36,28 +92,21 @@ function montarRespostas(resposta){
 
     const corDeFundoTitulo = perguntaNoDom.firstElementChild;
 
-    // console.log(corDeFundoTitulo);
     corDeFundoTitulo.style.backgroundColor = corPergunta;
 }
 
 function adicionaNoHtml(){
     
-    const perguntas = document.querySelector(".perguntas");
-    console.log(quizzSelecionado);
-
 
     mudaFundoQuizz(quizzSelecionado.image);
-    console.log(quizzSelecionado.questions);
     quizzSelecionado.questions.forEach((pergunta) => {
 
         corPergunta = pergunta.color;
-        tituloPergunta = pergunta.title;
-        console.log("COR DA PERGUNTA BACKGROUND COLOR: " + corPergunta);
-    
+        tituloPergunta = pergunta.title;    
 
         const respostas = pergunta.answers;
         respostas.sort( () => Math.random() - 0.5)
-        const templatePergunta = `<div class="pergunta">
+        const templatePergunta = `<div class="pergunta ${IdPergunta}">
                                     <div class="texto-pergunta"">
                                         <p>${tituloPergunta}</p>
                                     </div>
@@ -68,6 +117,8 @@ function adicionaNoHtml(){
         const perguntaNoDom = document.querySelector(".perguntas-container");
         perguntaNoDom.innerHTML += templatePergunta;
         respostas.forEach((resposta) => montarRespostas(resposta));
+
+        IdPergunta++
     });
 }
 
@@ -97,3 +148,5 @@ function obterQuizzes(){
 }
 
 obterQuizzes();
+
+
